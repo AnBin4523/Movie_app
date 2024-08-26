2744abd10e3a50ce6e61012e005feca5e91ea538
Ngô Lê Thiên Ân <quocthienhong@gmail.com>
2024-08-26 12:51:55 +0700

    push all

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/movie_app'),
+      dir: require('path').join(__dirname, './coverage/showtime'),
       subdir: '.',
       reporters: [
         { type: 'html' },
